c650fc714da9ef9090e4216e2730a79139a14c46
pulpt37 <manuelm794@gmail.com>
2018-09-08 11:50:11 -0500

    start of proyect

--- Angular/Proyecto_1/my-app/src/app/app.module.ts
+++ Angular/Proyecto_1/my-app/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import {MatInputModule} from '@angular/material/input';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   exports: [],
--- Nodos.cpp
+++ Nodos.cpp
@@ -10,12 +10,12 @@ int main(){
   printf("\nCuantos nodos tienes? ");
   scanf("%d",&num );
   int matriz_nodos[num][num];
-  
+
   int ruta[num];
   int pila[num];
   int aux_ruta=0;
   int aux_pila=0;
-
+//Lenado de matriz
   for(i=0;i<num;i++){
     for(j=0;j<num;j++){
       printf("Posicion actual: i:%d-j:%d\n",i,j);
@@ -30,6 +30,7 @@ int main(){
     }
     printf("\n");
   }
+  //se indica el inicio y final
   printf("Desde que nodo quieres empezar: ");
   scanf("%d",&inicio);
   ruta[aux_ruta]=inicio;
@@ -37,7 +38,7 @@ int main(){
   scanf("%d",&termino);
   j=0;
   i=inicio;
-  
+
   int aux;
   while (j<num) {
       if (i==termino) {
@@ -47,30 +48,30 @@ int main(){
     printf("Posicion actual: i:%d\tj:%d\n",i,j );
     printf("Valor en matriz: %d\n",matriz_nodos[i][j]);
     scanf("%d",&aux );
-  
+
     //Salto de linea->
 
     if(matriz_nodos[i][j]!=0){
       matriz_nodos[i][j]=0;
-      printf("vengo de : i:%d\tj:%d\n",i,j );
+      //printf("vengo de : i:%d\tj:%d\n",i,j );
       i=j;
       j=0;
+      printf("ruta actual:%d\n",ruta[aux_ruta]);
       aux_ruta++;
       ruta[aux_ruta]=i;
-
-      printf("Estoy en  : i:%d\tj:%d\n",i,j );
+      //printf("Estoy en  : i:%d\tj:%d\n",i,j );
       continue;
     }
     j++;
 
     //<-
   }
-  
+
 
   if(victoria==true){
     printf("Llegaste\n" );
-    for(i=0;i<aux_ruta-1;i++){
-      printf("Ruta: %d",ruta[i]);
+    for(i=0;i<aux_ruta+1;i++){
+      printf("Ruta num %d: %d \n",i,ruta[i]);
     }
   }else{
     printf("No se encontro camino\n");
